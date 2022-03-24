eed7775e3e92b92a3b7cc4177bd6551f46d1f4dc
Thomas Deutsch <thomas@tuxpeople.org>
2022-03-24 21:57:46 +0100

    various changes

--- files/bgimage.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-osascript -e 'tell application "System Events" to tell every desktop to set picture to "~/iCloudDrive/Multimedia/Backgounds/Desktop/luca-micheli-422053-unsplash.jpg"'
